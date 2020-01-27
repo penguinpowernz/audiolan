4b226663ac3f597b0693fb577bb79a754d2f1b36
Robert McLeod <robert@penguinpowernz.co.nz>
2020-01-27 15:43:27 +1300

    fixed bugs with client view

--- views/client.go
+++ views/client.go
@@ -3,6 +3,7 @@ package views
 import (
 	"fmt"
 	"log"
+	"strings"
 	"time"
 
 	"fyne.io/fyne"
@@ -24,36 +25,57 @@ func (cv *ClientView) Render() *fyne.Container {
 	cv.boxAddr = widget.NewEntry()
 	cv.boxAddr.SetPlaceHolder("Set server address")
 
-	btnConnect := widget.NewButton("Connect", func() {})
+	lblStatus := widget.NewLabel("Disconnected")
+	lblTx := widget.NewLabel("0 kB")
 
-	btnConnect.OnTapped = func() {
+	btnConnect := widget.NewButton("Connect", func() {})
+	updateButton := func() {
 		if cv.cl.ConnectedTo(cv.boxAddr.Text) {
-			log.Println("is connected, disconnecting")
-			cv.cl.Disconnect()
-			btnConnect.SetText("Connect")
-			cv.boxAddr.SetReadOnly(false)
-		} else {
-			log.Println("not connected, connecting")
-			cv.cl.ConnectTo(cv.boxAddr.Text)
 			cv.boxAddr.SetReadOnly(true)
 			btnConnect.SetText("Disconnect")
+		} else {
+			btnConnect.SetText("Connect")
+			cv.boxAddr.SetReadOnly(false)
 		}
 	}
 
-	lblStatus := widget.NewLabel("Disconnected")
-	lblTx := widget.NewLabel("0 kB")
+	btnConnect.OnTapped = func() {
+		if cv.boxAddr.Text == "" {
+			lblStatus.SetText("Invalid Address")
+			return
+		}
+
+		if cv.cl.ConnectedTo(cv.boxAddr.Text) {
+			log.Println("is connected, disconnecting")
+			cv.cl.Disconnect()
+		} else {
+			log.Println("not connected, connecting")
+			cv.cl.ConnectTo(cv.boxAddr.Text)
+		}
+
+		updateButton()
+	}
+
+	cv.cl.OnError(func(err error) {
+		if strings.Contains(err.Error(), "connection refused") {
+			lblStatus.SetText("Server down")
+		}
+	})
 
 	go func() {
 		txkB := 0.0
 		for {
+			updateButton()
 			time.Sleep(time.Second)
+
 			if cv.cl.Connected {
 				lblStatus.SetText(fmt.Sprintf("Connected for %0.fs", time.Since(cv.cl.ConnectedAt).Seconds()))
-				txkB += 44.1
+				txkB = cv.cl.BytesReceived() / 1024
 			} else {
 				txkB = 0
 				lblStatus.SetText("Disconnected")
 			}
+
 			lblTx.SetText(fmt.Sprintf("%0.1f kB", txkB))
 		}
 	}()
