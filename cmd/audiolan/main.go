0dcfa62ac4899242662eacfa770b8b0131bb24e3
Robert McLeod <robert@penguinpowernz.co.nz>
2020-02-02 21:47:44 +1300

    ensure portaudio is initialized

--- cmd/audiolan/main.go
+++ cmd/audiolan/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fyne.io/fyne/app"
 	"fyne.io/fyne/widget"
+	"github.com/gordonklaus/portaudio"
 	"github.com/penguinpowernz/audiolan"
 	"github.com/penguinpowernz/audiolan/views"
 )
@@ -11,6 +12,9 @@ func main() {
 	app := app.New()
 
 	w := app.NewWindow("AudioLAN")
+
+	portaudio.Initialize()
+	defer portaudio.Terminate()
 
 	client := audiolan.NewClient()
 	svr := audiolan.NewServer()
